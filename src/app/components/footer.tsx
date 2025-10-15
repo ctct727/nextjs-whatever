ad228d9eaf16686c87ad70c7282c358511e942cf
Chris <ctct727@gmail.com>
2025-10-14 21:19:09 -0400

    second

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <footer className="flex justify-center items-=center mt-auto border-black/10 border-t h-[40px] px-5">
+        <small>&copy; {new Date().getFullYear()} My Website. All rights reserved.</small>
+    </footer>
+);
+
+export default Footer;
